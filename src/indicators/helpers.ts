088566ad7870f3b3338b3f798e84cdbc02da6a31
clementpl <clement22860@gmail.com>
2019-03-04 16:25:20 +0100

    [indicator] MergeLabel helper refactor

--- src/indicators/helpers.ts
+++ src/indicators/helpers.ts
@@ -1,5 +1,8 @@
 // create a new object with every key prefixed by "$label-"
 export function mergeLabel(object: any, label: string): any {
+  if (typeof object === 'number' || typeof object === 'string') {
+    return { [label]: object };
+  }
   return Object.keys(object).reduce((acc: any, currentKey: string) => {
     acc[`${label}-${currentKey}`] = object[currentKey];
     return acc;
