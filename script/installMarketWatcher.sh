e88730f5f66d247f4d244aeba8dce734715a598b
clementpl <clement22860@gmail.com>
2019-03-20 09:34:33 +0100

    Add init influx-crypto-watcher script

---
+++ script/installMarketWatcher.sh
@@ -0,0 +1,9 @@
+# Clone project
+git clone https://github.com/clementpl/influx-crypto-watcher.git
+cd influx-crypto-watcher
+# Install
+npm install
+# Run influx
+npm run docker-start
+# Download init data
+npm run init-influx-test
